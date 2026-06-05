e9cab648b747b6ee8406421ceba805fb11c5ff9a
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-06-05 08:51:43 +0400

    Create 1 - Two Sum.cs

---
+++ leetcode/1 - Two Sum.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        
+        // [2,7,11,15]
+        // Target 9
+        for (int i = 0; i < nums.Length; i++) {
+            int temp = target - nums[i];
+
+            if (dict.ContainsKey(temp)) {
+                return new int[] {dict[temp], i};
+            }
+
+            dict[nums[i]] = i;
+        }
+
+        return new int[] {0, 0};
+    }
+}
